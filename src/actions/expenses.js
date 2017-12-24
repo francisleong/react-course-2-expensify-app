5e108be7ba62c32cd5675cc3005d970e5e147c6b
Francis Leong <francisleong2@gmail.com>
2017-12-24 02:30:14 -0500

    Add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,7 +32,6 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
-
 export const startRemoveExpense = ({ id } = {}) => {
   return (dispatch) => {
     return database.ref(`expenses/${id}`).remove().then(() => {
@@ -47,6 +46,14 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates));
+    })
+  }
+};
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
