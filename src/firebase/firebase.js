c820808e37c2766ab76ef9692b8d72e292c1f4fe
Francis Leong <francisleong2@gmail.com>
2017-12-24 01:17:32 -0500

    Fetch data from database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,131 +15,115 @@ const database = firebase.database();
 
 export { firebase, database as default };
 
-
-
-// const database = firebase.database();
-
+// // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
 //   console.log(snapshot.key, snapshot.val());
-// })
+// });
 
+// // child_changed
 // database.ref('expenses').on('child_changed', (snapshot) => {
 //   console.log(snapshot.key, snapshot.val());
-// })
+// });
 
+// // child_added
 // database.ref('expenses').on('child_added', (snapshot) => {
 //   console.log(snapshot.key, snapshot.val());
-// })
+// });
 
-// database.ref().on('value', () => {
-//   database.ref('expenses').once('value').then((snapshot) => {
-//     const expenses = [];
+// // database.ref('expenses')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const expenses = [];
 
-//     snapshot.forEach((childSnapshot) => {
-//       expenses.push({
-//         id: childSnapshot.key,
-//         ...childSnapshot.val()
-//       });
-//     });
+// //     snapshot.forEach((childSnapshot) => {
+// //       expenses.push({
+// //         id: childSnapshot.key,
+// //         ...childSnapshot.val()
+// //       });
+// //     });
 
-//     console.log(expenses);
-//   });
-// })
+// //     console.log(expenses);
+// //   });
 
-// database.ref('expenses')
-//   .once('value')
-//   .then((snapshot) => {
-//     const expenses = [];
+// // database.ref('expenses').on('value', (snapshot) => {
+// //   const expenses = [];
 
-//     snapshot.forEach((childSnapshot) => {
-//       expenses.push({
-//         id: childSnapshot.key,
-//         ...childSnapshot.val()
-//       });
-//     });
+// //   snapshot.forEach((childSnapshot) => {
+// //     expenses.push({
+// //       id: childSnapshot.key,
+// //       ...childSnapshot.val()
+// //     });
+// //   });
 
-//     console.log(expenses);
-//   });
+// //   console.log(expenses);
+// // });
 
 // database.ref('expenses').push({
 //   description: 'Rent',
 //   note: '',
 //   amount: 109500,
-//   createdAt: 989484651651
+//   createdAt: 976123498763
 // });
 
-// database.ref('notes').push({
-//   title: 'Course Topics',
-//   body: 'React Native, Angular, Pyhton'
-// });
-
-// database.ref('notes/-L0wUNmlBYeJHQrzMHbK').remove();
-
-// database.ref().on('value', (snapshot) => {
-//   const val = snapshot.val();
-//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company} `)
-// });
-
-// // No promises because it runs a single time only when this constantly runs
-// const onValueChange = database.ref().on('value', (snapshot) => {
-//   console.log(snapshot.val());
-// }, (e) => {
-//   console.log('Error with data fetching', e);
-// })
-
-// setTimeout(() => {
-//   database.ref('age').set(29)
-// }, 3500);
-
-// setTimeout(() => {
-//   database.ref().off(onValueChange);
-// }, 7000);
-
-// setTimeout(() => {
-//   database.ref('age').set(30)
-// }, 10500);
 
 
-// Getting data
-// database.ref('location/city')
-//   .once('value')
-//   .then((snapshot) => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   })
-//   .catch((e) => {
-//     console.log('Error fetching data', e);
-//   });
 
-// Setting data
-// database.ref().set({
-//   name: 'Francis Leong',
-//   age: 26,
-//   stressLevel: 6,
-//   job: {
-//     title: 'Software Developer',
-//     company: 'Google'
-//   },
-//   location: {
-//     city: 'Philadelphia',
-//     country: 'United States'
-//   }
-// }).then(() => {
-//   console.log('Data is saved');
-// }).catch((e) => {
-//   console.log('This failed.', e);
-// });
 
-// database.ref().update({
-//   stressLevel: 9,
-//   'job/company': 'Amazon',
-//   'location/city': 'Seattle'
-// });
 
-// database.ref('isSingle')
-//   .remove()
-//   .then(() => {
-//     console.log('Data was removed');
-//   }).catch((e) => {
-//     console.log('Data was not removed', e);
-//   });
+// // database.ref('notes/-Krll52aVDQ3X6dOtmS7').remove();
+
+// // database.ref('notes').push({
+// //   title: 'Course Topics',
+// //   body: 'React Native, Angular, Python'
+// // });
+
+// // database.ref().on('value', (snapshot) => {
+// //   const val = snapshot.val();
+// //   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// // })
+
+// // Setup data sub -> Andrew is a Software Developer at Amazon.
+
+// // Change the data and make sure it reprints
+
+// // database.ref('location/city')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(val);
+// //   })
+// //   .catch((e) => {
+// //     console.log('Error fetching data', e);
+// //   });
+
+// // database.ref().set({
+// //   name: 'Andrew Mead',
+// //   age: 26,
+// //   stressLevel: 6,
+// //   job: {
+// //     title: 'Software developer',
+// //     company: 'Google'
+// //   },
+// //   location: {
+// //     city: 'Philadelphia',
+// //     country: 'United States'
+// //   }
+// // }).then(() => {
+// //   console.log('Data is saved!');
+// // }).catch((e) => {
+// //   console.log('This failed.', e);
+// // });
+
+// // database.ref().update({
+// //   stressLevel: 9,
+// //   'job/company': 'Amazon',
+// //   'location/city': 'Seattle'
+// // });
+
+// // database.ref()
+// //   .remove()
+// //   .then(() => {
+// //     console.log('Data was removed');
+// //   }).catch((e) => {
+// //     console.log('Did not remove data', e);
+// //   });
